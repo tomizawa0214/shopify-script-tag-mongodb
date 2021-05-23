c1db7606e92e0491442ed23766c5048259e3826d
tomizawa0214 <j.tomizawa.japan@gmail.com>
2021-05-23 13:48:50 +0900

    import

--- public/test-script.js
+++ public/test-script.js
@@ -1,4 +1,5 @@
 // const APP_URl = 'https://shopifytopapp.herokuapp.com'
+import { APP_URl } from '../constant'
 
 const script = document.createElement('script')
 script.src = 'https://code.jquery.com/jquery-3.4.1.min.js'
